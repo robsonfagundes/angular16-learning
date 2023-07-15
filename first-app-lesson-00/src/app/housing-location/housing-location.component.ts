b5a9383458342b9e65e6257e74ba14a703661d9f
Robson Fagundes <robsonfagundes@gmail.com>
2023-07-15 19:04:49 -0300

    Lesson 5: Add an input parameter to the component

--- first-app-lesson-00/src/app/housing-location/housing-location.component.ts
+++ first-app-lesson-00/src/app/housing-location/housing-location.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housinglocation';
 
 @Component({
   selector: 'app-housing-location',
@@ -12,6 +13,7 @@ import { CommonModule } from '@angular/common';
   `,
   styleUrls: ['./housing-location.component.css']
 })
-export class HousingLocationComponent {
 
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
 }
